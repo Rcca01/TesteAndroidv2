9b75c2d62657eacbde8c7a69ee406bd3cbaa1bae
Raul Cardoso <cavalcante.cardoso@gmail.com>
2020-07-12 19:58:11 -0300

    start activity login

---
+++ app/src/main/java/com/example/accentureteste/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.accentureteste.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+        val success: LoggedInUserView? = null,
+        val error: Int? = null
+)
